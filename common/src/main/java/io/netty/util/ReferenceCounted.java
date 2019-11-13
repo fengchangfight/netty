fc465098e3e7426ce59747688e27b0c38d80d859
fengchang <fengchang_fight@163.com>
2019-11-13 09:30:24 +0800

    cmm

--- common/src/main/java/io/netty/util/ReferenceCounted.java
+++ common/src/main/java/io/netty/util/ReferenceCounted.java
@@ -15,24 +15,28 @@
  */
 package io.netty.util;
 
-/**fc comment
- * 一个接口，定义了需要实现的方法
- * A reference-counted object that requires explicit deallocation.
+/**
+ * fc comment 顾名思义即可，具体看用到处 A reference-counted object that requires explicit
+ * deallocation.
  * <p>
- * When a new {@link ReferenceCounted} is instantiated, it starts with the reference count of {@code 1}.
- * {@link #retain()} increases the reference count, and {@link #release()} decreases the reference count.
- * If the reference count is decreased to {@code 0}, the object will be deallocated explicitly, and accessing
- * the deallocated object will usually result in an access violation.
+ * When a new {@link ReferenceCounted} is instantiated, it starts with the
+ * reference count of {@code 1}. {@link #retain()} increases the reference
+ * count, and {@link #release()} decreases the reference count. If the reference
+ * count is decreased to {@code 0}, the object will be deallocated explicitly,
+ * and accessing the deallocated object will usually result in an access
+ * violation.
  * </p>
  * <p>
- * If an object that implements {@link ReferenceCounted} is a container of other objects that implement
- * {@link ReferenceCounted}, the contained objects will also be released via {@link #release()} when the container's
- * reference count becomes 0.
+ * If an object that implements {@link ReferenceCounted} is a container of other
+ * objects that implement {@link ReferenceCounted}, the contained objects will
+ * also be released via {@link #release()} when the container's reference count
+ * becomes 0.
  * </p>
  */
 public interface ReferenceCounted {
     /**
-     * Returns the reference count of this object.  If {@code 0}, it means this object has been deallocated.
+     * Returns the reference count of this object. If {@code 0}, it means this
+     * object has been deallocated.
      */
     int refCnt();
 
@@ -47,32 +51,36 @@ public interface ReferenceCounted {
     ReferenceCounted retain(int increment);
 
     /**
-     * Records the current access location of this object for debugging purposes.
-     * If this object is determined to be leaked, the information recorded by this operation will be provided to you
-     * via {@link ResourceLeakDetector}.  This method is a shortcut to {@link #touch(Object) touch(null)}.
+     * Records the current access location of this object for debugging purposes. If
+     * this object is determined to be leaked, the information recorded by this
+     * operation will be provided to you via {@link ResourceLeakDetector}. This
+     * method is a shortcut to {@link #touch(Object) touch(null)}.
      */
     ReferenceCounted touch();
 
     /**
-     * Records the current access location of this object with an additional arbitrary information for debugging
-     * purposes.  If this object is determined to be leaked, the information recorded by this operation will be
-     * provided to you via {@link ResourceLeakDetector}.
+     * Records the current access location of this object with an additional
+     * arbitrary information for debugging purposes. If this object is determined to
+     * be leaked, the information recorded by this operation will be provided to you
+     * via {@link ResourceLeakDetector}.
      */
     ReferenceCounted touch(Object hint);
 
     /**
-     * Decreases the reference count by {@code 1} and deallocates this object if the reference count reaches at
-     * {@code 0}.
+     * Decreases the reference count by {@code 1} and deallocates this object if the
+     * reference count reaches at {@code 0}.
      *
-     * @return {@code true} if and only if the reference count became {@code 0} and this object has been deallocated
+     * @return {@code true} if and only if the reference count became {@code 0} and
+     *         this object has been deallocated
      */
     boolean release();
 
     /**
-     * Decreases the reference count by the specified {@code decrement} and deallocates this object if the reference
-     * count reaches at {@code 0}.
+     * Decreases the reference count by the specified {@code decrement} and
+     * deallocates this object if the reference count reaches at {@code 0}.
      *
-     * @return {@code true} if and only if the reference count became {@code 0} and this object has been deallocated
+     * @return {@code true} if and only if the reference count became {@code 0} and
+     *         this object has been deallocated
      */
     boolean release(int decrement);
 }
